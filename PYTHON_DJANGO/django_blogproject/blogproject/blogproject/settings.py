2ef5d2f2f8bdcbcf6a774f7ca8f66e20bec6af53
min-18 <doleme00@gmail.com>
2022-06-27 16:32:48 +0900

    add secrets.json .gitignore files

--- PYTHON_DJANGO/django_blogproject/blogproject/blogproject/settings.py
+++ PYTHON_DJANGO/django_blogproject/blogproject/blogproject/settings.py
@@ -1,7 +1,10 @@
 
-import os
 
+import os
+import json
+import secrets
 from pathlib import Path
+from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,8 +13,25 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
+#보안파일
+secret_file = os.path.join(BASE_DIR, 'secrets.json')
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+    def get_secret(setting, secrets=secrets):
+        try:
+            return secrets[setting]
+
+        except KeyError:
+            error_msg = "에러"
+            raise ImproperlyConfigured(error_msg)
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y#olm*!r7b6$!v++x9j4h#0dn55aycdg0a&2t_j&!$x3ptk%f3'
+SECRET_KEY = get_secret("SECRET_KEY")
+#  'django-insecure-y#olm*!r7b6$!v++x9j4h#0dn55aycdg0a&2t_j&!$x3ptk%f3'
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
